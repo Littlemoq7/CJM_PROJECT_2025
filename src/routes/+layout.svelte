4495d264965aa8c10d5702ace54e73afeaa1ead7
Littlemoq7 <connormoquin@gmail.com>
2025-10-25 12:54:32 -0400

    Removed render children

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,14 @@
 	import Header from './Header.svelte';
 	import '../app.css';
 	
-	let { children } = $props();
+	
 </script>
 
 <div class="app">
 	<Header />
 
 	<main>
-		{@render children()}
+		<slot />
 	</main>
 
 	<footer>
